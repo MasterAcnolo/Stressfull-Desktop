6f66687162f3678614f2090a97e505692b2a4b12
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-02-20 19:45:24 +0100

    commit 2

--- index.py
+++ index.py
@@ -4,7 +4,6 @@ import tkinter as tk
 from tkinter import ttk
 import pygame
 
-# Initialiser pygame pour la musique
 pygame.mixer.init()
 pygame.mixer.music.load("song.mp3")
 pygame.mixer.music.play(loops=-1)
@@ -15,20 +14,33 @@ messages = [
     "Kernel System Error",
     "Votre Ordinateur va exploser",
     "Votre Carte Graphique est à +200°C, Vous pouvez y griller un oeuf si vous le désirez"
+    "Le fond d'écran est trop pixelisé, votre ordinateur ne peut plus le supporter."
+    "Votre ordinateur a décidé de faire une pause. Retour dans 10 minutes."
+    "Le système d'exploitation a pris une sieste imprévu... Redémarrage dans 3… 2… 1…"
+    "Vous êtes sur le point de devenir une légende... dans la file d'attente pour un support technique."
+    "Votre ordinateur a demandé à prendre des vacances. Il reviendra après un redémarrage."
+    "Qui lit ça?"
 ]
 
 def spawn_window():
     window = tk.Toplevel(root)
-    window.geometry(f"300x150+{choice(range(100, 800))}+{choice(range(100, 500))}")
+    message = choice(messages)
+    
+    width = 300
+    height = 150
+    window.geometry(f"{width}x{height}+{choice(range(100, 800))}+{choice(range(100, 500))}")
 
-    label = tk.Label(window, text=choice(messages), fg='red', font=("Helvetica", 12))
+    label = tk.Label(window, text=message, fg='red', font=("Helvetica", 12), wraplength=width-20)
     label.pack(pady=10)
 
     progress = ttk.Progressbar(window, orient="horizontal", length=250, mode="indeterminate")
     progress.pack(pady=5)
     progress.start(25)
 
-    Timer(3, window.destroy).start()
+    window.update_idletasks()  
+    window.geometry(f"{max(width, label.winfo_width()+20)}x{max(height, label.winfo_height()+40)}")
+
+    #Timer(10, window.destroy).start() <----- Pour que les fenêtres se ferment Xs après ouverture
     Timer(uniform(0.05, 0.2), spawn_window).start()
 
 root = tk.Tk()
